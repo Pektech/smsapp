f55fa5c20e2bac83a1a8dcf4a50cdaceac974c5f
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-05-29 01:00:12 -0400

    added create message form

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from wtforms import (Form, StringField, DateTimeField, SubmitField,
+                     validators as v)
+from wtforms.widgets import TextArea
+
+
+class ReminderForm(Form):
+    message = StringField("Message", validators=[v.length(min=1, max=160),
+                        v.InputRequired()], widget=TextArea())
+    send_on = DateTimeField("Send On", validators=[
+        v.InputRequired()], format='%Y/%m/%d %H:%M')
+    submit = SubmitField('Create')
+
+
