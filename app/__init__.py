1fa69b7c07b0b7903a74cb0d6b8b7ff8be37ca4e
Ric Leddy <14008446+Pektech@users.noreply.github.com>
2018-05-31 01:19:37 -0400

    set up celery heartbeat

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from .views import reminders
 from .extensions import db, login_manager, celery
 from .models import Reminder, User
 from .tasks import send_reminders
+from . import celery_config
 
 
 
@@ -30,6 +31,7 @@ def register_extensions(app):
     login_manager.init_app(app)
     celery.init_app(app)
 
+    celery.config_from_object(celery_config)
     login_manager.login_view = 'reminders.login'
 
     @login_manager.user_loader
--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Reminder(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     message = db.Column(db.String(160), nullable=False)
     send_on = db.Column(db.DateTime, nullable=False)
-    sent_on = db.Column(db.DateTime)
+    sent_on = db.Column(db.DateTime,)
 
     @property
     def status(self):
